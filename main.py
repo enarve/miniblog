087e0784bbb28e3a0ac755cd9ff6be2067f13d53
Nikita <80875524+enarve@users.noreply.github.com>
2026-04-20 11:35:08 +0200

    Sample templates

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# sample data
+posts = [
+    {
+        'author': 'enarve',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': '20 april 2026'
+    },
+    {
+        'author': 'fjarne',
+        'title': 'Good weather',
+        'content': 'Finally it is raining outside.',
+        'date_posted': '20 april 2026'
+    }
+]
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/index")
+def index():
+    return render_template('index.html', posts=posts)
+    
+@app.route("/about")
+def about():
+    return render_template('about.html')
+    
+if __name__ == '__main__':
+    app.run(debug=True)
