fe38dafa319ef7ec44d3cbbe75fc8fe367376802
Nikita <80875524+enarve@users.noreply.github.com>
2026-04-21 18:09:24 +0200

    More forms

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
@@ -29,14 +29,23 @@ def index():
 def about():
     return render_template('about.html', title='About')
     
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
     
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'admin' and form.password.data == '123':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+           flash('Login unsuccessful! Please check username and password.', 'danger') 
     return render_template('login.html', title='Login', form=form)
     
 if __name__ == '__main__':
