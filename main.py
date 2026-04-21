5b604b4cf6fe610a7bbceb05b507a8983fc4f2f9
Nikita <80875524+enarve@users.noreply.github.com>
2026-04-21 16:58:15 +0200

    Forms

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '26b001748d48a5d5df963f3be6f021d9'
 
 # sample data
 posts = [
@@ -26,6 +28,16 @@ def index():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+    
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+    
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
     
 if __name__ == '__main__':
     app.run(debug=True)
