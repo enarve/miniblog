193f1cc131798b6a2016a4de85fe1ebb20d206a3
Nikita <80875524+enarve@users.noreply.github.com>
2026-04-26 16:13:49 +0200

    Login and register

--- miniblog/__init__.py
+++ miniblog/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '26b001748d48a5d5df963f3be6f021d9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from miniblog import routes
--- miniblog/routes.py
+++ miniblog/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, flash, redirect
+from flask_login import login_user, current_user, logout_user
 
-from miniblog import app
+from miniblog import app, db, bcrypt
 from miniblog.forms import RegistrationForm, LoginForm
 from miniblog.models import User, Post
 
@@ -31,19 +32,33 @@ def about():
     
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in!', 'success')
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
     
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.username.data == 'admin' and form.password.data == '123':
-            flash('You have been logged in!', 'success')
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('index'))
         else:
-           flash('Login unsuccessful! Please check username and password.', 'danger') 
+            flash('Login unsuccessful! Please check username and password.', 'danger') 
     return render_template('login.html', title='Login', form=form)
+    
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
