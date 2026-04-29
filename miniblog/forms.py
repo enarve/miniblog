269a33e85c2cfde54f76e3fd789db83f7a3813b4
Nikita <80875524+enarve@users.noreply.github.com>
2026-04-29 23:18:59 +0200

    Account changes

--- miniblog/forms.py
+++ miniblog/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from flask_wtf.recaptcha.validators import ValidationError
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
@@ -13,9 +13,10 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign up')
     
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError('This username is taken. Please choose a different one.')
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('This username is taken. Please choose a different one.')
             
     def validate_email(self, email):
         email = User.query.filter_by(email=email.data).first()
@@ -27,3 +28,17 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign in')
+    
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Update')
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('This username is taken. Please choose a different one.')
+            
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('This email is taken. Please choose a different one.')
