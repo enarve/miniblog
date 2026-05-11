8901b61195910d73497d7406a3c2ee398f684558
Nikita <80875524+enarve@users.noreply.github.com>
2026-05-11 22:30:24 +0200

    Config and app structure

--- miniblog/__init__.py
+++ miniblog/__init__.py
@@ -1,29 +1,37 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '26b001748d48a5d5df963f3be6f021d9'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
+from miniblog.config import Config
+
+
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
 login_manager.login_view = 'users.login' # type: ignore
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
-mail = Mail(app)
 
-from miniblog.main.routes import main
-from miniblog.users.routes import users
-from miniblog.posts.routes import posts
+mail = Mail()
 
-app.register_blueprint(main)
-app.register_blueprint(users)
-app.register_blueprint(posts)
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    from miniblog.main.routes import main
+    from miniblog.users.routes import users
+    from miniblog.posts.routes import posts
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    
+    return app
--- miniblog/users/utils.py
+++ miniblog/users/utils.py
@@ -3,14 +3,15 @@ import os
 import secrets
 from PIL import Image
 from flask_mail import Message
+from flask import current_app
 
-from miniblog import app, mail
+from miniblog import mail
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_fn)
     output_size = (125, 125)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
